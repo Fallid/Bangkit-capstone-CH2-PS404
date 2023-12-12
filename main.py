cd628407403d0d6216e23f6d04ea06eea564dd93
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2023-12-12 21:52:02 +0700

    First Commit

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify, render_template, request
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = '202.10.40.73'
-app.config['MYSQL_USER'] = 'destiplanner'
-app.config['MYSQL_PASSWORD'] = 'kE<138£<m1Ly'
-app.config['MYSQL_DB'] = 'destiplanner'
 
+load_dotenv()
+
+# Database configuration
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
