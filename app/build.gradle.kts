305c6f323fd6a6c96d26c6538abf07dfa9fbb68b
Fallid <naufalopal002@gmail.com>
2023-12-14 17:14:28 +0700

    chore(gradle): Add Dependency Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.naufal.capstonech2ps404"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.naufal.capstonech2ps404"
@@ -26,6 +26,15 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -44,4 +53,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//    Compose dependency
+    implementation (platform("androidx.compose:compose-bom:2023.05.01"))
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.8.1")
+    implementation ("io.coil-kt:coil-compose:2.2.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    androidTestImplementation (platform("androidx.compose:compose-bom:2023.05.01"))
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("androidx.navigation:navigation-compose:2.7.5")
 }
