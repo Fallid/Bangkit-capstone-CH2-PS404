b954eb79b35c0dede6cfc260321ca6e072537107
Fallid <naufalopal002@gmail.com>
2023-12-16 12:02:37 +0700

    style: change primary color application

--- app/src/main/java/com/naufal/capstonech2ps404/style/AppTheme.kt
+++ app/src/main/java/com/naufal/capstonech2ps404/style/AppTheme.kt
@@ -24,7 +24,7 @@ private val darkColorScheme = darkColorScheme(
 private val lightColorScheme = lightColorScheme(
     primary = primaryColor,
     secondary = PurpleGrey80,
-    tertiary = Pink40,
+    tertiary = onSurface,
 
     /* Other default colors to override
         background = Color(0xFFFFFBFE),
