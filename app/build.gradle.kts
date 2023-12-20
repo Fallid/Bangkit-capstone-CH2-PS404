b679dffaa10087aeed7defe680fdb1a81eca1cd5
Fallid <naufalopal002@gmail.com>
2023-12-21 04:28:41 +0700

    chore(gradle): new dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,10 +65,12 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation ("androidx.activity:activity-compose:1.8.1")
     implementation ("io.coil-kt:coil-compose:2.2.2")
+    implementation("io.coil-kt:coil-svg:2.2.2")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     androidTestImplementation (platform("androidx.compose:compose-bom:2023.05.01"))
     androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation ("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
