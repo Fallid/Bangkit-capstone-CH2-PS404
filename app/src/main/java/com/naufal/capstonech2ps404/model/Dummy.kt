2d744f673caa9880b4673d1afbc011999c715203
Fallid <naufalopal002@gmail.com>
2023-12-21 01:15:43 +0700

    feat(ModelDummy): Add new item data dummy

--- app/src/main/java/com/naufal/capstonech2ps404/model/Dummy.kt
+++ app/src/main/java/com/naufal/capstonech2ps404/model/Dummy.kt
@@ -7,13 +7,20 @@ object Dummy {
             "Borobudur Temple",
             "Candi Borobudur adalah sebuah candi Buddha yang terletak di Magelang, Jawa Tengah, Indonesia. Candi ini merupakan salah satu keajaiban arsitektur dunia dan menjadi tempat ziarah agama Buddha.",
             "https://upload.wikimedia.org/wikipedia/commons/8/8c/Borobudur-Nothwest-view.jpg",
-            "Magelang"
+            "4.5 out of 5",
+            "43$",
+            "10day",
+            "Magelang",
+            true
         ),
         Vacation(
             "2",
             "Komodo Island",
             "Pulau Komodo terkenal karena menjadi rumah bagi kadal komodo, salah satu spesies kadal terbesar di dunia. Pulau ini juga menawarkan keindahan alam bawah laut yang menakjubkan.",
             "https://upload.wikimedia.org/wikipedia/commons/e/e3/Komodo_Island_north_aerial.jpg",
+            "4.5 out of 5",
+            "45$",
+            "10day",
             "Nusa Tenggara Timur"
         ),
         Vacation(
@@ -21,6 +28,9 @@ object Dummy {
             "Raja Ampat",
             "Raja Ampat adalah destinasi penyelaman yang sangat populer di Indonesia. Terkenal dengan keindahan terumbu karangnya, Raja Ampat menjadi surga bagi para penyelam dan pecinta alam bawah laut.",
             "https://upload.wikimedia.org/wikipedia/commons/8/88/Raja_Ampat%2C_Mutiara_Indah_di_Timur_Indonesia.jpg",
+            "4.5 out of 5",
+            "45$",
+            "10day",
             "Papua Barat"
         ),
         Vacation(
@@ -28,49 +38,76 @@ object Dummy {
             "Tana Toraja",
             "Tana Toraja adalah daerah di Sulawesi Selatan yang terkenal dengan kebudayaan uniknya, termasuk rumah adat berbentuk tongkonan dan upacara pemakaman yang khas.",
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbpepmrARs2ZPnDfQbaOcVtQWT-KkZVtRFpffPGns3Cg&s",
+            "4.5 out of 5",
+            "45$",
+            "11day",
             "Sulawesi Selatan"
         ),
+
         Vacation(
             "5",
             "Bali",
             "Pulau Bali adalah destinasi wisata terkenal di Indonesia. Dikenal dengan pantainya yang indah, budaya yang kaya, dan kehidupan malam yang ramai.",
             "https://upload.wikimedia.org/wikipedia/commons/2/25/Pura_Ulun_Danu_Bratan_A.JPG",
+            "4.5 out of 5",
+            "45$",
+            "11day",
             "Bali"
         ),
+
         Vacation(
             "6",
             "Gunung Bromo",
             "Gunung Bromo adalah gunung berapi yang terkenal di Jawa Timur, Indonesia. Pemandangan matahari terbit di atas lautan pasir dan langit yang dipenuhi asap menjadikan Bromo salah satu destinasi wisata favorit.",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Bromo-Semeru-Batok-Widodaren.jpg/800px-Bromo-Semeru-Batok-Widodaren.jpg",
+            "4.5 out of 5",
+            "45$",
+            "11day",
             "Probolinggo"
         ),
+
         Vacation(
             "7",
             "Danau Toba",
             "Danau Toba adalah danau vulkanik terbesar di dunia, terletak di Sumatra Utara. Pulau Samosir di tengah danau menambah pesona alam dan budaya khas Batak Toba.",
             "https://upload.wikimedia.org/wikipedia/commons/a/ae/Indonesia_-_Lake_Toba_%2826224127503%29.jpg",
+            "4.5 out of 5",
+            "45$",
+            "11day",
             "Samosir"
         ),
+
         Vacation(
             "8",
             "Pantai Pink",
             "Pantai Pink terletak di Pulau Komodo dan terkenal karena pasirnya yang berwarna merah muda. Pantai ini menawarkan pemandangan alam yang memesona dan snorkeling yang menakjubkan.",
             "https://upload.wikimedia.org/wikipedia/commons/1/1e/Pink_Beach%2C_Padar_Island%2C_Komodo_National_Park.jpg",
+            "4.5 out of 5",
+            "45$",
+            "11day",
             "Nusa Tenggara Barat"
         ),
+
         Vacation(
             "9",
             "Pulau Weh",
             "Pulau Weh adalah pulau terluar di Indonesia yang terletak di ujung barat Sumatra. Pulau ini dikenal dengan keindahan bawah lautnya dan menjadi surganya para penyelam.",
             "https://phinemo.com/wp-content/uploads/2015/11/Teupin-Layeu.jpg",
+            "4.5 out of 5",
+            "45$",
+            "11day",
             "Lore Ipsum"
         ),
+
         Vacation(
             "10",
             "Prambanan Temple",
             "Candi Prambanan adalah kompleks candi Hindu terbesar di Indonesia, terletak di Yogyakarta. Candi ini merupakan contoh arsitektur candi Hindu yang megah dan indah.",
             "https://upload.wikimedia.org/wikipedia/commons/0/0b/Prambanan_Temple_Yogyakarta_Indonesia.jpg",
+            "4.5 out of 5",
+            "45$",
+            "10day",
             "Lore Ipsum"
-        ),
+        )
     )
 }
