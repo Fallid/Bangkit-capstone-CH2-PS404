3649a2b75eddd11a94fa84485e8624b70b9cffd6
Fallid <naufalopal002@gmail.com>
2023-12-22 06:23:38 +0700

    chore(gradle): Allowing http scurity and retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -83,5 +84,11 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation ("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation ("io.coil-kt:coil-compose:1.4.0")
 
+    // Retrofit Dependency
+    implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
